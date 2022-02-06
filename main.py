6045a924fffd9d8d2aa2b2f933320cb4129219a1
dhaven <dev_ddh@pm.me>
2022-02-06 17:23:45 +0000

    track final deck for each player

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 from antlr4 import *
-from StarRealmsLexer import StarRealmsLexer
-from StarRealmsParser import StarRealmsParser
-from StarRealmsVisitor import StarRealmsVisitor
+from python.StarRealmsLexer import StarRealmsLexer
+from python.StarRealmsParser import StarRealmsParser
+from python.StarRealmsVisitor import StarRealmsVisitor
 
 class Card:
     CARD_SCOUT = 0
@@ -10,24 +10,41 @@ class Card:
     CARD_SUPPLY_BOT = 2
     CARD_EXPLORER = 3
     CARD_FEDERATION_SHUTTLE = 4
+    CARD_PATROL_MECH = 5
+    CARD_ROYAL_REDOUBT = 6
 
     def __init__(self, cardType=0, count=1):
         self.cardType = cardType # the name of this card
         self.count = count # the number of copies of this card
+        self.playedCount = 0
+        self.scrappedCount = 0
+
+    def addCount(self, count):
+        self.count += count
+
+    def addPlayed(self, count):
+        self.playedCount += count
+    
+    def addScrappedCount(self, count):
+        self.scrappedCount += count
 
     def addNameFromString(self, cardName):
-        if(cardName == "SupplyBot"):
+        if cardName == "SupplyBot":
             self.cardType = Card.CARD_SUPPLY_BOT
-        if(cardName == "FederationShuttle"):
+        if cardName == "FederationShuttle":
             self.cardType = Card.CARD_FEDERATION_SHUTTLE
-        if(cardName == "Explorer"):
+        if cardName == "Explorer":
             self.cardType = Card.CARD_EXPLORER
-        if(cardName == "Scout"):
+        if cardName == "Scout":
             self.cardType = Card.CARD_SCOUT
-        if(cardName == "Viper"):
+        if cardName == "Viper":
             self.cardType = Card.CARD_VIPER
+        if cardName == "PatrolMech":
+            self.cardType = Card.CARD_PATROL_MECH
+        if cardName == "RoyalRedoubt":
+            self.cardType = Card.CARD_ROYAL_REDOUBT
     
-    def __str__(self):
+    def getString(self):
         if self.cardType == Card.CARD_SUPPLY_BOT:
             return "Supply Bot"
         elif self.cardType == Card.CARD_SCOUT:
@@ -38,36 +55,67 @@ class Card:
             return "Federation Shuttle"
         elif self.cardType == Card.CARD_EXPLORER:
             return "Explorer"
+        elif self.cardType == Card.CARD_PATROL_MECH:
+            return "Patrol Mech"
+        elif self.cardType == Card.CARD_ROYAL_REDOUBT:
+            return "Royal Redoubt"
         else:
             return "noname"
+    
+    def __str__(self):
+        return self.getString() + "-- count: " + str(self.count) + ", played times: " + str(self.playedCount) + ", scrapped times: " + str(self.scrappedCount)
 
 class Deck:
     
-    def __init__(self, cards):
-        self.cards = cards
-
-# information representing a player during a game
-class Player:
-
-    def __init__(self, deck, name):
-        self.deck = deck # the cards this plqyer has
-        self.name = name # the nqme of this player
+    def __init__(self):
+        self.cards = dict()
+        cardScout = Card(Card.CARD_SCOUT, 8)
+        cardViper = Card(Card.CARD_VIPER, 2)
+        self.cards[cardScout.getString()] = cardScout
+        self.cards[cardViper.getString()] = cardViper
+    
+    def update(self, actions):
+        for action in actions:
+            if action.type == Action.ACTION_PURCHASE:
+                for card in action.purchasedCards:
+                    if card.getString() not in self.cards:
+                        self.cards[card.getString()]= card
+                    else:
+                        self.cards[card.getString()].addCount(1)
+            elif action.type == Action.ACTION_PLAY:
+                for card in action.playedCards:
+                    self.cards[card.getString()].addPlayed(1)
+            elif action.type == Action.ACTION_SCRAP:
+                for card in action.scrappedCards:
+                    self.cards[card.getString()].addScrappedCount(1)
+            else:
+                pass
+    
+    def __str__(self):
+        cardList = ""
+        for card in self.cards:
+            cardList += str(self.cards[card]) + "\n"
+        return cardList
+                
 
 class Action:
     ACTION_PURCHASE = 0
     ACTION_PLAY = 1
     ACTION_ATTACK = 2
-    POOL_COMBAT = 3
-    POOL_TRADE = 4
+    ACTION_SCRAP = 3
+    POOL_COMBAT = 4
+    POOL_TRADE = 5
 
-    def __init__(self, actionType=0):
-        self.actionType = actionType
+    def __init__(self):
+        self.type = Action.ACTION_PURCHASE
         self.addedPoolCombat = 0
         self.removedPoolCombat = 0
         self.addedPoolTrade = 0
         self.removedPoolTrade = 0
+        self.authority = 50
         self.playedCards = list()
         self.purchasedCards = list()
+        self.scrappedCards = list()
     
     def addToPool(self,poolCategory, amount):
         if poolCategory == Action.POOL_COMBAT:
@@ -85,21 +133,54 @@ class Action:
         else:
             pass
     
+    def changeAuthority(self, amount):
+        self.authority += amount
+    
+    def getString(self):
+        if self.type == Action.ACTION_PURCHASE:
+            return "Purchase"
+        elif self.type == Action.ACTION_PLAY:
+            return "Play"
+        elif self.type == Action.ACTION_ATTACK:
+            return "Attack"
+        else:
+            return "Other"
+        
     def __str__(self):
-        action = "action type: " + str(self.actionType) + "\n"
+        action = "action type: " + self.getString() + "\n"
+        action += "Authority: "
+        action += str(self.authority)
+        action += "\n"
         action += "Pool balance \n"
         action += "addedPoolCombat: " + str(self.addedPoolCombat) + ", removedPoolCombat: " + str(self.removedPoolCombat) + ", addedPoolTrade: " + str(self.addedPoolTrade) + ", removedPoolTrade: " + str(self.removedPoolTrade) + "\n"
-        action += "Played Cards: \n"
-        for card in self.playedCards:
-            action += str(card)
-            action += "\n"
-        action += "Purchased Cards: \n"
-        for card in self.purchasedCards:
-            action += str(card)
-            action += "\n"
+        # action += "Played Cards: \n"
+        # for card in self.playedCards:
+        #     action += str(card)
+        #     action += "\n"
+        # action += "Purchased Cards: \n"
+        # for card in self.purchasedCards:
+        #     action += str(card)
+        #     action += "\n"
+        # action += "Scrapped Cards: \n"
+        # for card in self.scrappedCards:
+        #     action += str(card)
+        #     action += "\n"
         return action
     
+
+# information representing a player during a game
+class Player:
+
+    def __init__(self, name):
+        self.deck = Deck() # the cards this player has which depends on first or second
+        self.name = name # the name of this player
     
+    def isPlayer(self, name):
+        return self.name == name
+    
+    def __str__(self):
+        return self.name + " with deck: \n" + str(self.deck) + "\n"
+
 
 # information representing a round in a game
 class Round:
@@ -121,12 +202,24 @@ class Battle:
       self.rounds = list() # the list of rounds played in the game
       self.players = list() # the list of players involved in the game
     
+    def updatePlayer(self, name, actions):
+        for player in self.players:
+            if player.isPlayer(name):
+                player.deck.update(actions)
+                return
+        nextPlayer = Player(name)
+        nextPlayer.deck.update(actions)
+        self.players.append(nextPlayer)
+
+    
     def __str__(self):
         battle = "Battle started! \n"
         for round in self.rounds:
             battle += str(round)
             battle += "\n"
             battle += "next round! \n"
+        for player in self.players:
+            battle += str(player)
         return battle
 
 class MyVisitor(StarRealmsVisitor):
@@ -136,9 +229,11 @@ class MyVisitor(StarRealmsVisitor):
         for turn in ctx.turn():
             nextRound = self.visit(turn)
             battle.rounds.append(nextRound)
+            battle.updatePlayer(nextRound.player, nextRound.actions)
         return battle
 
     # for each turn, get the summary of action and the user who performed it
+    # grammar: action+ endTurn NEWLINE ;
     def visitTurn(self, ctx):
         nextRound = Round(list(),self.visit(ctx.endTurn()))
         for action in ctx.action():
@@ -147,6 +242,7 @@ class MyVisitor(StarRealmsVisitor):
         return nextRound
 
     # endTurn subtree has the name of the player who played this turn
+    # grammar: WORD+ turnCount ;
     def visitEndTurn(self, ctx):
         playerName = ""
         for word in ctx.WORD():
@@ -157,36 +253,55 @@ class MyVisitor(StarRealmsVisitor):
             playerName += " "
         return playerName
 
+    # grammar: summaryAction NEWLINE actionDetail* ;
     def visitAction(self, ctx):
         nextAction = Action()
         if ctx.summaryAction().purchase():
-            nextAction.actionType = Action.ACTION_PURCHASE
+            nextAction.type = Action.ACTION_PURCHASE
             nextAction.purchasedCards.append(self.visit(ctx.summaryAction().purchase()))
         elif ctx.summaryAction().attack():
-            nextAction.actionType = Action.ACTION_ATTACK
-        elif  ctx.summaryAction().getText().startswith("Play"):
-            nextAction.actionType = Action.ACTION_PLAY
+            nextAction.type = Action.ACTION_ATTACK
+        elif  ctx.summaryAction().play():
+            nextAction.type = Action.ACTION_PLAY
+        elif ctx.summaryAction().scrap():
+            nextAction.type = Action.ACTION_SCRAP
         else:
-            exit(1)
+            exit("error")
         for actionDetail in ctx.actionDetail():
-            summary = self.visit(actionDetail)
-            nextAction.addToPool(Action.POOL_TRADE,summary['effect']['addedPoolTrade'])
-            nextAction.addToPool(Action.POOL_COMBAT,summary['effect']['addedPoolCombat'])
-            nextAction.removeFromPool(Action.POOL_TRADE,summary['effect']['removedPoolTrade'])
-            nextAction.removeFromPool(Action.POOL_COMBAT,summary['effect']['removedPoolCombat'])
-            if summary['card']:
-                nextAction.playedCards.append(summary['card'])
+            if actionDetail.newBalanceDetail():
+                summary = self.visit(actionDetail.newBalanceDetail())
+                nextAction.addToPool(Action.POOL_TRADE,summary['effect']['addedPoolTrade'])
+                nextAction.addToPool(Action.POOL_COMBAT,summary['effect']['addedPoolCombat'])
+                nextAction.removeFromPool(Action.POOL_TRADE,summary['effect']['removedPoolTrade'])
+                nextAction.removeFromPool(Action.POOL_COMBAT,summary['effect']['removedPoolCombat'])
+                nextAction.changeAuthority(summary['effect']['authority'])
+                if summary['card']:
+                    nextAction.playedCards.append(summary['card'])
+            elif actionDetail.scrapDetail():
+                scrappedCard = self.visit(actionDetail.scrapDetail())
+                nextAction.scrappedCards.append(scrappedCard)
+            else:
+                exit("error")
         return nextAction
-    
-    def visitActionDetail(self, ctx):
+
+    # grammar: name SEPARATOR card? effect balance ;
+    def visitNewBalanceDetail(self, ctx):
         card = None
         if ctx.card():
             card = self.visit(ctx.card())
         effect = self.visit(ctx.effect())
         return dict(card=card,effect=effect)
+    
+    # grammar: SCRAPPED card ;
+    def visitScrapDetail(self, ctx):
+        card = None
+        if ctx.card():
+            card = self.visit(ctx.card())
+        return card
 
+    # grammar: : (INCREMENT | DECREASE) category ;
     def visitEffect(self, ctx):
-        effect = dict(addedPoolTrade=0, removedPoolTrade=0, addedPoolCombat=0, removedPoolCombat=0)
+        effect = dict(addedPoolTrade=0, removedPoolTrade=0, addedPoolCombat=0, removedPoolCombat=0, authority=0)
         category = self.visit(ctx.category())
         if ctx.INCREMENT() and category == "Trade":
             effect['addedPoolTrade'] = int(ctx.INCREMENT().getText().strip('+'))
@@ -196,11 +311,18 @@ class MyVisitor(StarRealmsVisitor):
             effect['addedPoolCombat'] = int(ctx.INCREMENT().getText().strip('+'))
         elif ctx.DECREASE() and category == "Combat":
             effect['removedPoolCombat'] = int(ctx.DECREASE().getText().strip('-'))
+        elif ctx.INCREMENT() and category == "Authority":
+            effect['authority'] = int(ctx.INCREMENT().getText().strip('+'))
+        elif ctx.DECREASE() and category == "Authority":
+            effect['authority'] = 0 - int(ctx.DECREASE().getText().strip('-'))
         else:
-            exit(1)
+            exit("error")
         return effect
 
     def visitPurchase(self, ctx):
+        return self.visit(ctx.card())
+
+    def visitScrap(self, ctx):
         return self.visit(ctx.card())
 
     def visitCategory(self, ctx):
