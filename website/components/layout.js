e2a35c8d2e383bb10c148cc53b23468598b3044d
dhaven <dev_ddh@pm.me>
2022-04-13 21:09:41 +0100

    remove default meta data

--- website/components/layout.js
+++ website/components/layout.js
@@ -15,16 +15,9 @@ export default function Layout({ children}) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="Statistics about your Star Realms games"
         />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className="flex flex-col items-end gap-1">
         <button type="button" className="bg-white hover:bg-gray-100 font-medium rounded-full text-sm p-2.5 m-3 text-center inline-flex items-center" onClick={openModal}>
