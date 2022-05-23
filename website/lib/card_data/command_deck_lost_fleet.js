e398700f2e5cc328e12d3d49d0b4a44750ea18de
LumanBrovis <106066045+LumanBrovis@users.noreply.github.com>
2022-05-23 11:39:25 +0200

    Create command_deck_lost_fleet.js

---
+++ website/lib/card_data/command_deck_lost_fleet.js
@@ -0,0 +1,62 @@
+export const command_deck_lost_fleet = {
+    "name": "command_deck_lost_fleet",
+    "cards": {
+        "assaultshard": {
+            "name": "Assault Shard",
+            "cost": 0,
+            "faction": [
+                "Unaligned",
+            ],
+            "type": "ship"
+        },
+        "commandshard": {
+            "name": "Command Shard",
+            "cost": 0,
+            "faction": [
+                "Unaligned",
+            ],
+            "type": "ship"
+        },
+        "jochumsgranddesign": {
+            "name": "Jochum's Grand Design",
+            "cost": 0,
+            "faction": [
+                "Unaligned",
+            ],
+            "type": "gambit"
+        },
+            "lostdreadnaught": {
+                "name": "Lost Dreadnaught",
+                "cost": 8,
+                "faction": [
+                    "Unaligned",
+                ],
+                "type": "ship"
+        },
+        "reconshard": {
+            "name": "Recon Shard",
+            "cost": 0,
+            "faction": [
+                "Unaligned",
+            ],
+            "type": "ship"
+        },
+        "salvageshard": {
+            "name": "Salvage Shard",
+            "cost": 0,
+            "faction": [
+                "Unaligned",
+            ],
+            "type": "ship"
+        },
+        "transportshard": {
+            "name": "Transport Shard",
+            "cost": 0,
+            "faction": [
+                "Unaligned",
+            ],
+            "type": "ship"
+
+        }
+    }
+}
