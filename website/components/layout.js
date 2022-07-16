08776a5c46a30a8486b9634565ee38bd06e3db85
dhaven <dev_ddh@pm.me>
2022-07-16 18:32:40 +0100

    remove duplicated component

--- website/components/layout.js
+++ website/components/layout.js
@@ -48,7 +48,6 @@ export default function Layout({ children }) {
             <svg className="w-12 h-12" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
         </button>
       </div>
-      <AddGameModal isOpen={isAddGameOpen} setIsOpen={setAddGameIsOpen}></AddGameModal>
       <NewFeatureModal isOpen={isNewFeatureOpen} setIsOpen={setIsNewFeatureOpen}></NewFeatureModal>
       <AddGameModal isOpen={isAddGameOpen} setIsOpen={setAddGameIsOpen}></AddGameModal>
     </div>
