ccfce243cd7945affbb63f776e6810e17ec733e3
dhaven <dev_ddh@pm.me>
2022-11-03 14:40:03 +0100

    fix add game modal

--- website/components/landingPage.js
+++ website/components/landingPage.js
@@ -1,67 +1,81 @@
 import Image from 'next/image'
 import SRLogInput from './SRLogInput.js'
+import AddGameModal from './addGameModal.js'
+import { useState } from 'react'
 
-export default function LandingPage(){
+export default function LandingPage() {
+    let [isAddGameOpen, setAddGameIsOpen] = useState(false)
+    function openAddGameModal() {
+        setAddGameIsOpen(true)
+    }
     return (
         <div className="flex w-full h-full m-1 p-1 lg:p-4">
-        <div className="flex flex-col h-full w-full lg:w-1/2 gap-4 p-2">
-            <div className="bg-white lg:p-4 p-2 w-full rounded-lg drop-shadow-scifi5">
-                <p className="text-2xl md:text-4xl font-light text-scifi5 text-center">
-                    Statistics for all your Star Realms games in just 3 steps
-                </p>
-            </div>
-            <div className="bg-white rounded-lg flex flex-col w-full h-full grow gap-4 lg:p-4 p-2 drop-shadow-scifi5">
-                <div className="flex grow items-center justify-center">
-                    <div>
-                        <p className="text-xl font-medium text-scifi5 m-2 p-2">1.</p>
+            <div className="flex flex-col h-full w-full lg:w-1/2 gap-4 p-2">
+                <div className="bg-white lg:p-4 p-2 w-full rounded-lg drop-shadow-scifi5">
+                    <p className="text-2xl md:text-4xl font-light text-scifi5 text-center">
+                        Statistics for all your Star Realms games in just 3 steps
+                    </p>
+                </div>
+                <div className="bg-white rounded-lg flex flex-col w-full h-full grow gap-4 lg:p-4 p-2 drop-shadow-scifi5">
+                    <div className="flex grow items-center justify-center">
+                        <div>
+                            <p className="text-xl font-medium text-scifi5 m-2 p-2">1.</p>
+                        </div>
+                        <div className="relative w-full lg:w-1/2 h-full m-2 p-2">
+                            <Image
+                                src="/images/1.png" // Route of the image file
+                                alt="Step 1"
+                                layout="fill"
+                                objectFit="contain"
+                            />
+                        </div>
                     </div>
-                    <div className="relative w-full lg:w-1/2 h-full m-2 p-2">
-                        <Image
-                            src="/images/1.png" // Route of the image file
-                            alt="Step 1"
-                            layout="fill"
-                            objectFit="contain"
-                        />
+                    <div className="flex grow items-center justify-center">
+                        <div>
+                            <p className="text-xl font-medium text-scifi5 m-2 p-2">2.</p>
+                        </div>
+                        <div className="relative w-full lg:w-1/2 h-full m-2 p-2">
+                            <Image
+                                src="/images/2.png" // Route of the image file
+                                alt="Step 1"
+                                layout="fill"
+                                objectFit="contain"
+                            />
+                        </div>
+                    </div>
+                    <div className="flex grow items-center justify-center">
+                        <div>
+                            <p className="text-xl font-medium text-scifi5 m-2 p-2">3.</p>
+                        </div>
+                        <div className="relative w-full lg:w-1/2 h-full m-2 p-2">
+                            <Image
+                                src="/images/3.png" // Route of the image file
+                                alt="Step 1"
+                                layout="fill"
+                                objectFit="contain"
+                            />
+                        </div>
                     </div>
                 </div>
-                <div className="flex grow items-center justify-center">
-                    <div>
-                        <p className="text-xl font-medium text-scifi5 m-2 p-2">2.</p>
-                    </div>
-                    <div className="relative w-full lg:w-1/2 h-full m-2 p-2">
-                        <Image
-                            src="/images/2.png" // Route of the image file
-                            alt="Step 1"
-                            layout="fill"
-                            objectFit="contain"
-                        />
-                    </div>
+            </div>
+            <div className="hidden md:flex md:flex-col w-full h-full w-1/2 gap-4 p-2">
+                <div className="bg-white rounded-lg lg:p-4 w-full drop-shadow-scifi5">
+                    <p className="text-4xl font-light text-scifi5 text-center">
+                        Add your game here 👇
+                    </p>
                 </div>
-                <div className="flex grow items-center justify-center">
-                    <div>
-                        <p className="text-xl font-medium text-scifi5 m-2 p-2">3.</p>
-                    </div>
-                    <div className="relative w-full lg:w-1/2 h-full m-2 p-2">
-                        <Image
-                            src="/images/3.png" // Route of the image file
-                            alt="Step 1"
-                            layout="fill"
-                            objectFit="contain"
-                        />
-                    </div>
+                <div className="bg-white flex rounded-lg flex-col w-full h-full grow gap-1 drop-shadow-scifi5">
+                    <SRLogInput></SRLogInput>
                 </div>
             </div>
-        </div>
-        <div className="hidden md:flex md:flex-col w-full h-full w-1/2 gap-4 p-2">
-            <div className="bg-white rounded-lg lg:p-4 w-full drop-shadow-scifi5">
-                <p className="text-4xl font-light text-scifi5 text-center">
-                    Add your game here 👇
-                </p>
+            <div className="z-20 sm:hidden fixed bottom-0 left-0 right-0">
+                <div className="flex justify-end">
+                    <button type="button" onClick={openAddGameModal} className="m-3 bg-scifi3 border border-scifi4 ring-scifi-2 drop-shadow-md hover:ring font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center">
+                        <svg className="w-8 h-8" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                    </button>
+                </div>
             </div>
-            <div className="bg-white flex rounded-lg flex-col w-full h-full grow gap-1 drop-shadow-scifi5">
-                <SRLogInput></SRLogInput>
-            </div>
-        </div>
+            <AddGameModal isOpen={isAddGameOpen} setIsOpen={setAddGameIsOpen}></AddGameModal>
         </div>
     )
 }
--- website/pages/game/[id].js
+++ website/pages/game/[id].js
@@ -2,7 +2,7 @@ import PlayerOverview from '../../components/playerOverview'
 import DeckOverview from '../../components/deckOverview'
 import Head from 'next/head'
 import Layout, { siteTitle } from '../../components/layout'
-import { Tab } from '@headlessui/react'
+import AddGameModal from '../../components/addGameModal.js'
 import { Fragment, useState } from 'react'
 import AuthorityChart from '../../components/authorityChart'
 import OtherCharts from '../../components/otherCharts'
@@ -18,6 +18,10 @@ export default function Game({ winner, extensions, events, players, winCondition
     const router = useRouter()
     let [activePlayer, setActivePlayer] = useState(winner)
     let [statsTab, setStatsTab] = useState("player") //can be either player or game
+    let [isAddGameOpen, setAddGameIsOpen] = useState(false)
+    function openAddGameModal() {
+        setAddGameIsOpen(true)
+    }
     let [open, setOpen] = useState(true)
     let updatVisibleDeck = function (player) {
         if (player == activePlayer) {
@@ -114,26 +118,27 @@ export default function Game({ winner, extensions, events, players, winCondition
                 }
                 <div className="z-20 sm:hidden sticky bottom-0 left-0 right-0">
                     <div className="flex justify-end">
-                        <button type="button" className="m-3 bg-scifi3 border border-scifi4 ring-scifi-2 drop-shadow-md hover:ring font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center">
+                        <button type="button" onClick={openAddGameModal} className="m-3 bg-scifi3 border border-scifi4 ring-scifi-2 drop-shadow-md hover:ring font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center">
                             <svg className="w-8 h-8" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
                         </button>
                     </div>
                     <div className="flex justify-around bg-white">
                         <button onClick={() => setStatsTab("player")} className={
-                            statsTab == "player" ? "bg-scifi3 text-white text-md border-2 border-scifi4 font-medium py-2 px-6 m-1 sm:rounded-lg"
-                                : "bg-scifi1 ring-scifi2 hover:ring border-2 border-scifi4 text-scifi4 text-md border-2 border-scifi4 font-medium py-2 px-6 m-1 sm:rounded-lg"
+                            statsTab == "player" ? "flex justify-center grow bg-scifi3 text-white text-md border-2 border-scifi4 font-medium py-2 px-6 m-1 sm:rounded-lg"
+                                : "flex justify-center grow bg-scifi1 ring-scifi2 hover:ring border-2 border-scifi4 text-scifi4 text-md border-2 border-scifi4 font-medium py-2 px-6 m-1 sm:rounded-lg"
                         }>
                             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
                         </button>
                         <button onClick={() => setStatsTab("game")} className={
-                            statsTab == "game" ? "bg-scifi3 text-white text-md border-2 border-scifi4 font-medium py-2 px-6 m-1 sm:rounded-lg"
-                                : "bg-scifi1 ring-scifi2 hover:ring border-2 border-scifi4 text-scifi4 text-md border-2 border-scifi4 font-medium py-2 px-6 m-1 sm:rounded-lg"
+                            statsTab == "game" ? "flex justify-center grow bg-scifi3 text-white text-md border-2 border-scifi4 font-medium py-2 px-6 m-1 sm:rounded-lg"
+                                : "flex justify-center grow bg-scifi1 ring-scifi2 hover:ring border-2 border-scifi4 text-scifi4 text-md border-2 border-scifi4 font-medium py-2 px-6 m-1 sm:rounded-lg"
                         }>
                             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>
                         </button>
                     </div>
                 </div>
             </div>
+            <AddGameModal isOpen={isAddGameOpen} setIsOpen={setAddGameIsOpen}></AddGameModal>
         </Layout>
     )
 }
