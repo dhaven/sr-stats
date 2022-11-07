a3b3418e0acbadbf50410c8634e417fe0409f97d
dhaven <dev_ddh@pm.me>
2022-11-07 16:19:34 +0100

    scale down username

--- website/components/playerOverview.js
+++ website/components/playerOverview.js
@@ -6,9 +6,9 @@ import Tooltip from './ui/tooltip.js'
 function PlayerOverview({name, deckData, authority, missions}) {
     return (
         <div className="flex items-stretch justify-between 2xl:gap-4">
-            <div className="flex flex-col">
+            <div className="flex grow-0 flex-col">
                 <div className="text-center">
-                    <p className="sm:p-2 text-3xl text-scifi5 font-bold whitespace-nowrap">{name}</p>
+                    <p className="sm:p-2 text-xl md:text-3xl text-scifi5 font-bold whitespace-nowrap">{name}</p>
                 </div>
                 <div className="flex justify-center">
                     <FinalAuthority authority={authority}></FinalAuthority>
@@ -26,7 +26,7 @@ function PlayerOverview({name, deckData, authority, missions}) {
                     }
                 </div>
             </div>
-            <div className="flex h-36 w-36 sm:h-48 sm:w-48 md:h-36 md:w-36 lg:h-40 lg:w-40 xl:w-48 wl:h-48 2xl:h-56 2xl:w-56">
+            <div className="flex h-32 w-32 sm:h-48 sm:w-48 md:h-36 md:w-36 lg:h-40 lg:w-40 xl:w-48 wl:h-48 2xl:h-56 2xl:w-56">
                 <FactionPie factions={deckData['factions']}></FactionPie>
             </div>
         </div>
