9ab710e9aba0cda278b2dd445a3d1600ec5ac8a9
dhaven <dev_ddh@pm.me>
2022-11-09 17:49:39 +0000

    update correct share onclick button

--- website/components/gameSummary.js
+++ website/components/gameSummary.js
@@ -16,12 +16,20 @@ function GameSummary({winner, loser, winCondition, extensions}){
         }
     }
     let shareGame = async function(){
-        if(true){
+        if(navigator.canShare({
+            url: window.location.href,
+            title: "Share game"
+        })){
             navigator.share({
                 url: window.location.href,
                 title: "Share game"
             }).then(() =>{
                 return true
+            })
+        }else{
+            navigator.clipboard.writeText(window.location.href).then(() => {
+                setVisi(true)
+                return false
             })
         }
 
@@ -74,7 +82,7 @@ function GameSummary({winner, loser, winCondition, extensions}){
                         })
                     }
                 </div>
-                <button onClick={(e) => {navigator.clipboard.writeText(window.location.href).then(() => setVisi(true))}} className="inline-flex font-medium bg-white text-scifi5 text-sm text-bold p-1 lg:px-4 lg:py-2 border items-center drop-shadow-md border-scifi4 ring-scifi-2 hover:ring rounded-lg active:bg-white/40" type="button">
+                <button onClick={shareGame} className="inline-flex font-medium bg-white text-scifi5 text-sm text-bold p-1 lg:px-4 lg:py-2 border items-center drop-shadow-md border-scifi4 ring-scifi-2 hover:ring rounded-lg active:bg-white/40" type="button">
                     <svg class="w-6 h-6 mr-2 -ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path>
                     </svg>
