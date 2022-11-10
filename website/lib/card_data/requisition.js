2dd17fc3010064adaf93a027e28195af361700e1
dhaven <dev_ddh@pm.me>
2022-11-10 15:30:46 +0000

    add support for requisition extension

---
+++ website/lib/card_data/requisition.js
@@ -0,0 +1,133 @@
+export const requisition = {
+    "name": "requisition",
+    "cards": {
+        "corsair": {
+            "name": "Corsair",
+            "cost": 3,
+            "faction": [
+                "Trade Federation"
+            ],
+            "type": "ship",
+            "metadata": {
+                "extension": "requisition",
+                "image": {
+                    "width": "x",
+                    "height": "y",
+                    "filename": "x.jpg"
+                }
+            }
+        },
+        "operationsplatform": {
+            "name": "Operations Platform",
+            "cost": 3,
+            "faction": [
+                "Machine Cult"
+            ],
+            "type": "base",
+            "metadata": {
+                "extension": "requisition",
+                "image": {
+                    "width": "x",
+                    "height": "y",
+                    "filename": "x.jpg"
+                }
+            }
+        },
+        "swarmcolony": {
+            "name": "Swarm Colony",
+            "cost": 6,
+            "faction": [
+                "Blob"
+            ],
+            "type": "base",
+            "metadata": {
+                "extension": "requisition",
+                "image": {
+                    "width": "x",
+                    "height": "y",
+                    "filename": "x.jpg"
+                }
+            }
+        },
+        "thearmory": {
+            "name": "The Armory",
+            "cost": 7,
+            "faction": [
+                "Star Empire"
+            ],
+            "type": "base",
+            "metadata": {
+                "extension": "requisition",
+                "image": {
+                    "width": "x",
+                    "height": "y",
+                    "filename": "x.jpg"
+                }
+            }
+        },
+        "warriorpod": {
+            "name": "Warrior Pod",
+            "cost": 2,
+            "faction": [
+                "Blob"
+            ],
+            "type": "ship",
+            "metadata": {
+                "extension": "requisition",
+                "image": {
+                    "width": "x",
+                    "height": "y",
+                    "filename": "x.jpg"
+                }
+            }
+        },
+        "hellfire": {
+            "name": "Hellfire",
+            "cost": 4,
+            "faction": [
+                "Star Empire"
+            ],
+            "type": "ship",
+            "metadata": {
+                "extension": "requisition",
+                "image": {
+                    "width": "x",
+                    "height": "y",
+                    "filename": "x.jpg"
+                }
+            }
+        },
+        "lunarlanding": {
+            "name": "Lunar Landing",
+            "cost": 9,
+            "faction": [
+                "Trade Federation"
+            ],
+            "type": "base",
+            "metadata": {
+                "extension": "requisition",
+                "image": {
+                    "width": "x",
+                    "height": "y",
+                    "filename": "x.jpg"
+                }
+            }
+        },
+        "tankermech": {
+            "name": "Tanker Mech",
+            "cost": 5,
+            "faction": [
+                "Machine Cult"
+            ],
+            "type": "ship",
+            "metadata": {
+                "extension": "requisition",
+                "image": {
+                    "width": "x",
+                    "height": "y",
+                    "filename": "x.jpg"
+                }
+            }
+        },
+    }
+}
