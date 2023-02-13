2e54645a636e1f41d110f7b2eab2818c1cff3895
dhaven <dev_ddh@pm.me>
2023-02-13 12:49:46 +0100

    add discord link on mobile

--- website/pages/game/[id].js
+++ website/pages/game/[id].js
@@ -200,6 +200,20 @@ export default function Game({ winner, loser, extensions, events, players, winCo
                         ></PlayerDetail>
                     )
                 }
+                <div className="flex justify-start mx-2">
+                    <a href="https://discord.gg/q4kqH775FA" target="_blank">
+                        <div className="flex bg-white rounded-lg py-2 px-1 border-black border">
+                            <div className="flex items-center mx-2">
+                                <Image
+                                    src={discord_img}
+                                    height="24"
+                                    width="24"
+                                />
+                            </div>
+                            <p>Join the community! </p>
+                        </div>
+                    </a>
+                </div>
                 <div className="z-20 sticky bottom-0 left-0 right-0">
                     {!isAddGameOpen &&
                         <div className="flex justify-end">
